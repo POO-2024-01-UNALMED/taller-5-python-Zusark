da864a87b471e2de39b9a0164eb6fb7ce3c1348d
Zusark <149200499+Zusark@users.noreply.github.com>
2024-03-19 11:47:03 -0500

    prueba 1

---
+++ zooAnimales/animal.py
@@ -0,0 +1,55 @@
+import zooAnimales
+class Animal():
+    _totalAnimales = 0
+
+    def __init__(self, nombre, edad, habitat, genero):
+        self._nombre = nombre
+        self._edad = edad
+        self._habitat = habitat
+        self._genero = genero
+        self._zona = None
+        Animal._totalAnimales += 1
+
+    def movimiento(self):
+        return "desplazarse"
+    
+    @classmethod
+    def totalPorTipo(cls):
+        return "Mamiferos : " + str(zooAnimales.mamifero.Mamifero.cantidadMamiferos()) + "\nAves : " + str(zooAnimales.ave.Ave.cantidadAves()) + "\nReptiles : " + str(zooAnimales.reptil.Reptil.cantidadReptiles()) + "\nPeces : " + str(zooAnimales.pez.Pez.cantidadPeces()) + "\nAnfibios : " + str(zooAnimales.anfibio.Anfibio.cantidadAnfibios())
+    
+    def toString(self):
+        if self._zona == None:
+            return "Mi nombre es " + str(self._nombre) + ", tengo una edad de " + str(self._edad) + ", habito en " + str(self._habitat) + " y mi genero es " + str(self._genero)
+        else:
+            return "Mi nombre es " + str(self._nombre) + ", tengo una edad de " + str(self._edad) + ", habito en " + str(self._habitat) + " y mi genero es " + str(self._genero) + ", la zona en la que me ubico es " + str(self._zona) + ", en el " + str(self._zona.getZoo().getNombre())
+
+    @classmethod    
+    def getTotalAnimales(cls):
+        return Animal._totalAnimales
+    
+    @classmethod
+    def setTotalAnimales(cls, total):
+        Animal._totalAnimales = total
+
+    def getNombre(self):
+        return self._nombre
+    
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getEdad(self):
+        return self._edad
+    
+    def setEdad(self, edad):
+        self._edad = edad
+
+    def getHabitat(self):
+        return self._habitat
+    
+
+
+    def getZona(self):
+        return self._zona
+    
+    def setZona(self, zona):
+        self._zona = zona
